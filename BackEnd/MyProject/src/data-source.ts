0ec7c66ea04837a580199835fb3586abf4efeeed
Malik fajar <devfajarmalik@gmail.com>
2023-11-21 01:23:09 +0700

    first commit

--- BackEnd/MyProject/src/data-source.ts
+++ BackEnd/MyProject/src/data-source.ts
@@ -4,13 +4,13 @@ import { DataSource } from "typeorm"
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
-    port: 5432,
+    host: "monorail.proxy.rlwy.net",
+    port: 17828,
     username: "postgres",
-    password: "MF13012003",
-    database: "threads",
+    password: "2FaGbcCC2b6eFGdCEcAAgAb1gb*Agf-a",
+    database: "railway",
     synchronize: true,
-    logging: false,
+    logging: true,
     entities: ["src/entities/*.ts"],
     migrations: ["src/migration/*.ts"],
     subscribers: [],
